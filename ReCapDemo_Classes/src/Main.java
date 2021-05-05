87d658f23e8a3a41618a0228a3adb44cd403268b
melek Dedik <melekdedik@gmail.com>
2021-05-06 02:41:06 +0300

    ReCapDemo App

---
+++ ReCapDemo_Classes/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		DortIslem dortIslem = new DortIslem();
+		
+		int sonuc = dortIslem.Topla(10, 78);
+		System.out.println(sonuc);
+
+	}
+
+}
