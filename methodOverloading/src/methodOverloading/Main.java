81f6a73e3518abd9553bf090f38ba44eed44071e
melek Dedik <melekdedik@gmail.com>
2021-05-06 02:35:03 +0300

    Method Overloading App

---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,13 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.Topla(8, 21));
+		
+		System.out.println(dortIslem.Topla(9, 11, 5));
+
+	}
+
+}
