18a238b290e66a4282897cde85de719c3f90bb90
melek Dedik <melekdedik@gmail.com>
2021-05-17 17:37:13 +0300

    Interface Uygulaması

---
+++ interfaceDemo/src/interfaceDemo/IEatable.java
@@ -0,0 +1,8 @@
+package interfaceDemo;
+
+public interface IEatable {
+	void eat();
+		
+	
+
+}
---
+++ interfaceDemo/src/interfaceDemo/Worker.java
@@ -0,0 +1,23 @@
+package interfaceDemo;
+//Bizim çalýþanýmýz
+public class Worker implements IWorker, IEatable, IPayable{
+
+	@Override
+	public void work() {
+		
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pay() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
